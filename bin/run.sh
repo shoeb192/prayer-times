385b3e96ba3b9f36a9b2209a3b2867560586bc94
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-08-18 23:40:55 +0200

    add control on ~/Desktop/site.txt

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-url=`cat ~/Desktop/site.txt`
+if [ -f ~/Desktop/site.txt ]; then
+    url=`cat ~/Desktop/site.txt`
+fi
+
+if [ -z "$url" ]; then
+    url="http://localhost"
+fi
+
 chromium-browser --app=$url --start-maximized &
 sleep 30
 xdotool key F11
