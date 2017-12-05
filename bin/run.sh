2310cbe303b01fa10bfb129a06ac4ab7741293c3
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-12-05 16:50:20 +0100

    best run handling

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+localUrl=http://localhost
+
 if [ -f ~/Desktop/site.txt ]; then
-    url=`cat ~/Desktop/site.txt`
+    localUrl=`cat ~/Desktop/site.txt`
 fi
 
-if [ -z "$url" ]; then
-    url="http://localhost"
+if [ -f ~/Desktop/local_site.txt ]; then
+    localUrl=`cat ~/Desktop/local_site.txt`
+fi
+
+if [ -f ~/Desktop/online_site.txt ]; then
+    onlineUrl=`cat ~/Desktop/online_site.txt`
+fi
+
+wget -q --spider https://google.com
+
+if [ $? -eq 0 ]; then
+    url=$onlineUrl
+else
+    url=$localUrl
 fi
 
 chromium-browser --app=$url --start-maximized &
